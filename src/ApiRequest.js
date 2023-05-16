c954e150a5ed2b4c3180d8e1aee4d5a4959d5aa8
Ajish s <ajish.r@infospica.com>
2023-05-16 09:30:24 +0530

    useeffect

---
+++ src/ApiRequest.js
@@ -0,0 +1,9 @@
+const ApiRequest = async (url = '', optionsObj = null, errMsg = null) => {
+  try {
+    const response = await fetch(url)
+  } catch (err) {
+
+  }
+}
+
+export default ApiRequest
--- src/App.js
+++ src/App.js
@@ -4,38 +4,42 @@ import Header from './Header';
 // import Content from './Content';
 import Footer from './Footer';
 import Content2 from './Content2';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import AddItem from './AddItem';
 import SearchItem from './SearchItem';
 
 function App() {
 
-  const [items, setItem] = useState(JSON.parse(localStorage.getItem('todo_list')));
-
+  const API_URL = "http://localhost:3500/items";
+  const [items, setItem] = useState([]);
   const [newitem, setnewitem] = useState('')
   const [search, setSearch] = useState('')
+  const [fetchError, setFetchError] = useState(null)
+  const [isLoading, setIsLoading] = useState(true)
+
+
 
   const addItem = (item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1
     const addNewItem = { id, checked: false, item }
     const listItems = [...items, addNewItem]
     setItem(listItems)
-    localStor(listItems)
+    // localStor(listItems)
   }
 
   const handleCheck = (id) => {
     const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item)
     setItem(listItems)
-    localStor(listItems)
+    // localStor(listItems)
   }
   const handleDelete = (id) => {
     const listItems = items.filter((item) => item.id !== id)
     setItem(listItems)
-    localStor(listItems)
+    // localStor(listItems)
   }
-  const localStor = (listItems) => {
-    localStorage.setItem('todo_list', JSON.stringify(listItems))
-  }
+  // const localStor = (listItems) => {
+  //   localStorage.setItem('todo_list', JSON.stringify(listItems))
+  // }
   const handleSubmit = (e) => {
     e.preventDefault()
     if (!newitem) return;
@@ -43,6 +47,26 @@ function App() {
     addItem(newitem)
     setnewitem("")
   }
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(API_URL)
+        if (!response.ok) throw Error("Data Not Recevied")
+        const listItems = await response.json()
+        setItem(listItems)
+        setFetchError(null)
+      }
+      catch (error) {
+        setFetchError(error.message);
+      } finally {
+        setIsLoading(false)
+      }
+    }
+    setTimeout(() => {
+      (async () => await fetchItems())()
+    }, 2000);
+
+  }, [])
 
 
   return (
@@ -59,11 +83,15 @@ function App() {
           search={search}
           setSearch={setSearch}
         />
-        <Content2
-          items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
-          handleCheck={handleCheck}
-          handleDelete={handleDelete}
-        />
+        <main>
+          {isLoading && <p>Loading Items...</p>}
+          {fetchError && <p>Error :{fetchError}</p>}
+          {!isLoading && !fetchError && <Content2
+            items={items.filter(item => ((item.item).toLowerCase()).includes(search.toLowerCase()))}
+            handleCheck={handleCheck}
+            handleDelete={handleDelete}
+          />}
+        </main>
         <Footer itemLength={items.length} />
       </header>
     </div>
