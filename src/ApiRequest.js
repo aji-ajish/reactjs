9f8ff4aac036fc3aceef3eecd75779bfaa5405b1
Ajish s <ajish.r@infospica.com>
2023-05-22 15:29:05 +0530

    update

--- src/ApiRequest.js
+++ src/ApiRequest.js
@@ -1,8 +1,12 @@
 const ApiRequest = async (url = '', optionsObj = null, errMsg = null) => {
   try {
-    const response = await fetch(url)
-  } catch (err) {
+    const response = await fetch(url, optionsObj )
+    if (!response.ok) throw Error('Please Reload the app')
 
+  } catch (err) {
+    errMsg = err.Message
+  } finally {
+return errMsg
   }
 }
 
--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Content2 from './Content2';
 import { useEffect, useState } from 'react';
 import AddItem from './AddItem';
 import SearchItem from './SearchItem';
+import ApiRequest from './ApiRequest';
 
 function App() {
 
@@ -19,22 +20,49 @@ function App() {
 
 
 
-  const addItem = (item) => {
+  const addItem = async(item) => {
     const id = items.length ? items[items.length - 1].id + 1 : 1
     const addNewItem = { id, checked: false, item }
     const listItems = [...items, addNewItem]
     setItem(listItems)
+
+    const postOption ={
+      method:'POST',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify(addNewItem)
+    }
+    const result=await ApiRequest(API_URL,postOption)
+    if(result)setFetchError(result)
     // localStor(listItems)
   }
 
-  const handleCheck = (id) => {
+  const handleCheck = async(id) => {
     const listItems = items.map((item) => item.id === id ? { ...item, checked: !item.checked } : item)
     setItem(listItems)
+
+    const myItem=listItems.filter((item)=>item.id===id)
+    const updateOption ={
+      method:'PATCH',
+      headers:{
+        'Content-Type':'application/json'
+      },
+      body:JSON.stringify({checked:myItem[0].checked})
+    }
+    const reqUrl=`${API_URL}/${id}`
+    const result=await ApiRequest(reqUrl,updateOption)
+    if(result)setFetchError(result)
     // localStor(listItems)
   }
-  const handleDelete = (id) => {
+  const handleDelete = async(id) => {
     const listItems = items.filter((item) => item.id !== id)
     setItem(listItems)
+
+    const deleteOption={method:'DELETE'}
+    const reqUrl=`${API_URL}/${id}`
+    const result=await ApiRequest(reqUrl,deleteOption)
+    if(result)setFetchError(result)
     // localStor(listItems)
   }
   // const localStor = (listItems) => {
@@ -57,7 +85,7 @@ function App() {
         setFetchError(null)
       }
       catch (error) {
-        setFetchError(error.message);
+        setFetchError(error.Message);
       } finally {
         setIsLoading(false)
       }
